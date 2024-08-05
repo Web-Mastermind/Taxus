fbe231d50765a300e88e9503782d3aaa6fdda49f
Web-Mastermind <AZEAgent@gmail.com>
2024-08-05 23:33:59 +0400

    added new items

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -9,15 +9,35 @@ const page = () => {
     <main className={styles.main}>
       <div className="container">
         <div className={styles.mainItems}>
-          <h1 className={styles.mainTitle}>{t("title")}</h1>
-          <div className={styles.mainTexts}>
-            <Image className={styles.mainIcon} src="/assets/icons/arrow.svg" alt="Arrow Icon" width={55} height={53} />
-            <h2 className={styles.mainSubTitle}>{t("subtitle")}</h2>
+          <div className={styles.leftContainer}>
+            <h1 className={styles.mainTitle}>{t("title")}</h1>
+            <div className={styles.mainTexts}>
+              <Image className={styles.mainIcon} src="/assets/icons/arrow.svg" alt="Arrow Icon" width={55} height={53} />
+              <h2 className={styles.mainSubTitle}>{t("subtitle")}</h2>
+            </div>
+            <h2 className={styles.mainSubSecondTitle}>{t("text")}</h2>
+            <p className={styles.mainTextItem}>{t("secontext")}</p>
+            <div className={styles.buttonContainer}>
+              <ContactUs />
+            </div>
           </div>
-          <h2 className={styles.mainSubSecondTitle}>{t("text")}</h2>
-          <p className={styles.mainTextItem}>{t("secontext")}</p>
-          <div className={styles.buttonContainer}>
-            <ContactUs />
+          <div className={styles.rightContainer}>
+            <div className={styles.mainRightItems}>
+              <p className={styles.mainRightTitle}>10+ Years</p>
+              <p className={styles.mainRightText}>of experience with IT</p>
+            </div>
+            <div className={styles.mainRightItems}>
+              <p className={styles.mainRightTitle}>&gt; 250</p>
+              <p className={styles.mainRightText}>partners in the network</p>
+            </div>
+            <div className={styles.mainRightItems}>
+              <p className={styles.mainRightTitle}>&gt; 230</p>
+              <p className={styles.mainRightText}>clients worldwide</p>
+            </div>
+            <div className={styles.mainRightItems}>
+              <p className={styles.mainRightTitle}>&gt; 150</p>
+              <p className={styles.mainRightText}>projects launched</p>
+            </div>
           </div>
         </div>
       </div>
