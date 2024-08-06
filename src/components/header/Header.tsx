e1e6c8bef876c98d173ada167fd5203b6432d7aa
Web-Mastermind <AZEAgent@gmail.com>
2024-08-07 02:39:23 +0400

    code optimisation

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,9 +2,12 @@ import Image from "next/image"
 import styles from "./style.module.css"
 import Link from "next/link"
 import LocalSwitcher from "../local-switcher"
+import { navLinks } from "@/links/Links"
 import { useTranslations } from "next-intl"
 
+
 const Header = () => {
+  const links = navLinks();
   const t = useTranslations("Header")
   return (
     <header className={styles.header}>
@@ -13,11 +16,13 @@ const Header = () => {
           <Link className={styles.siteLogoLink} href="/"><Image className={styles.logo} src="/assets/icons/SiteLogo.svg" alt="Site Logo" width={163} height={47.13} /></Link>
           <ul className={styles.headerLinks}>
             <li className={styles.headerLinkItems}>
-              <Link className={`${styles.headerLinksItem} ${styles.fonts}`} href="/">{t("services")}</Link>
-              <Link className={`${styles.headerLinksItem} ${styles.fonts}`} href="/">{t("banking")}</Link>
-              <Link className={`${styles.headerLinksItem} ${styles.fonts}`} href="/">{t("about")}</Link>
-              <Link className={`${styles.headerLinksItem} ${styles.fonts}`} href="/">{t("blog")}</Link>
-              <Link className={`${styles.headerLinksItem} ${styles.fonts}`} href="/">{t("contact")}</Link>
+              {
+                links.map(({ id, to, title }) => {
+                  return (
+                    <Link key={id} className={`${styles.headerLinksItem} ${styles.fonts}`} href={to}>{title}</Link>
+                  )
+                })
+              }
             </li>
             <Link className={`${styles.logInLink} ${styles.fonts}`} href="/">{t("login")}</Link>
             <LocalSwitcher />
