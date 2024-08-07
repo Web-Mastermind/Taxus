3bdc7375e9afa8699bcfb6f893cb4df754273f18
Web-Mastermind <AZEAgent@gmail.com>
2024-08-08 00:45:38 +0400

    added language switch

--- src/components/sectionServer/Serve.tsx
+++ src/components/sectionServer/Serve.tsx
@@ -2,20 +2,23 @@ import Image from "next/image"
 import styles from "./style.module.css"
 import { serveSection } from "@/links/Links"
 import ContactUs from "../mainbutton/ContactUs"
+import { useTranslations } from "next-intl"
 
 const Serve = () => {
+  const serve = serveSection();
+  const t = useTranslations("serveSection")
   return (
     <section className={styles.serveSection}>
       <div className="container">
         <div className={styles.sectionItems}>
           <div className={styles.sectionLeftContainer}>
-            <p className={styles.sectionText}>Who we serve</p>
-            <h2 className={styles.sectionTitle}>The areas of our expertise are as diverse as the verticals of IT business are</h2>
+            <p className={styles.sectionText}>{t("firstTexts.serveMainText")}</p>
+            <h2 className={styles.sectionTitle}>{t("firstTexts.serveMainTitle")}</h2>
             <Image className={styles.sectionImage} src="/assets/img/BusinessPeople.png" alt="Business People Image" width={480} height={300} />
           </div>
           <div className={styles.sectionRightContainer}>
             {
-              serveSection.map(({ id, icon, text }) => {
+              serve.map(({ id, icon, text }) => {
                 return (
                   <div key={id} className={styles.rightContainerItems}>
                     <span className={styles.diamondIcon}>{icon}</span>
