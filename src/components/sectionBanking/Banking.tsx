a51d070103d03b874852b86510c8efbe7d96bba7
Web-Mastermind <AZEAgent@gmail.com>
2024-08-09 23:58:11 +0400

    new section Banking

---
+++ src/components/sectionBanking/Banking.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image"
+import styles from "./style.module.css"
+import sectionImage from "../../../public/assets/img/BankingImage.png"
+import { bankingSection } from "@/links/Links"
+import Link from "next/link"
+import { useTranslations } from "next-intl"
+
+const Banking = () => {
+  const t = useTranslations("bankingSection")
+  const banking = bankingSection();
+  return (
+    <article className={styles.sectionBanking}>
+      <div className={styles.sectionItems}>
+        <Image className={styles.bankingImage} src={sectionImage} alt="phone and credit card image" />
+        <div className="container">
+          <div className={styles.bankingTexts}>
+            <p className={styles.bankingMainText}>{t("sectionText")}</p>
+            <h3 className={styles.bankingMainTitle}>{t("sectionTitle")}</h3>
+            {
+              banking.map(({ id, icon, text }) => {
+                return (
+                  <div key={id} className={styles.bankingTextContainer}>
+                    <span className={styles.textDecor}>{icon}</span>
+                    <p className={styles.bankingSubTexts}>{text}</p>
+                  </div>
+                )
+              })
+            }
+            <Link className={styles.bankinLinkItem} href="/">{t("bankingLink")} &rarr;</Link>
+          </div>
+        </div>
+      </div>
+    </article>
+  )
+}
+
+export default Banking
