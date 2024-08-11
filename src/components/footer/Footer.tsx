9ce7dc2b56d774a40a4d1068adc18147c7e40164
Web-Mastermind <AZEAgent@gmail.com>
2024-08-11 22:52:28 +0400

    footer WIP

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import styles from "./style.module.css"
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className="container">
+        <div className={styles.footerItems}>
+
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
