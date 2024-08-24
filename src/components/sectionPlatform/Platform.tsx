8531ef13b3e2ecf1e0629dfd86dea0d4eff7b5e3
Web-Mastermind <azeagent@gmail.com>
2024-08-24 21:09:44 +0400

    fixed link translate

--- src/components/sectionPlatform/Platform.tsx
+++ src/components/sectionPlatform/Platform.tsx
@@ -13,7 +13,7 @@ const Platform = () => {
             <p className={styles.platformText}>{t("sectionText")}</p>
             <h2 className={styles.platformTitle}>{t("sectionTitle")}</h2>
             <p className={styles.platformSubText}>{t("subText")}</p>
-            <Link className={styles.platformLinkItem} href="/">{t("paltformLink")} &rarr;</Link>
+            <Link className={styles.platformLinkItem} href="/">{t("platformLink")} &rarr;</Link>
           </div>
           <div className={styles.sectionImage}>
             <Image className={styles.platformImage} src="/assets/img/PlatformSectionImg.png" alt="young woman image" width={536} height={480} />
