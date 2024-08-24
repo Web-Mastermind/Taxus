0f8bdac8b38e9c4b67bab825445a58a2506c3e0f
Web-Mastermind <azeagent@gmail.com>
2024-08-24 23:34:09 +0400

    new component Industries

---
+++ src/components/aboutSectionIndustries/Industries.tsx
@@ -0,0 +1,37 @@
+import { aboutSectionIndustries } from "@/links/Links"
+import ContactUs from "../mainbutton/ContactUs"
+import styles from "./style.module.css"
+import { useTranslations } from "next-intl"
+
+const Industries = () => {
+  const t = useTranslations("aboutSectionIndustries")
+  const industries = aboutSectionIndustries();
+  return (
+    <section className={styles.sectionIndustries}>
+      <div className="container">
+        <div className={styles.sectionItems}>
+          <div className={styles.sectionLeftItems}>
+            <p className={styles.sectionLeftText}>{t("IndustriesText")}</p>
+            <h3 className={styles.sectionLeftTitle}>{t("IndustriesTitle")}</h3>
+            <p className={styles.sectionLeftSubText}>{t("IndustriesSubText")}</p>
+            <ContactUs />
+          </div>
+          <div className={styles.sectionRightItemsContainer}>
+            {
+              industries.map(({ id, title, text }) => {
+                return (
+                  <div key={id} className={styles.sectionRightItems}>
+                    <h4 className={styles.sectionRightTitle}>{title}</h4>
+                    <p className={styles.sectionRightText}>{text}</p>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Industries
