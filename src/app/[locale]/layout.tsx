b563c2873138a9455dd3b3fefa84e54a7e730afe
Web-Mastermind <azeagent@gmail.com>
2024-08-24 21:07:15 +0400

    added next intl client provider

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,14 +1,28 @@
-import Footer from "@/components/footer/Footer"
-import Header from "@/components/header/Header"
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import "../globals.css"
+import Header from '@/components/header/Header';
+import Footer from '@/components/footer/Footer';
 
-const layout = ({ children }: { children: React.ReactNode }) => {
+export default async function LocaleLayout({
+  children,
+  params: { locale }
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+
+  const messages = await getMessages();
+
   return (
-    <div>
-      <Header />
-      {children}
-      <Footer />
-    </div>
-  )
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          <Header locale={locale} />
+          {children}
+          <Footer />
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
 }
-
-export default layout
