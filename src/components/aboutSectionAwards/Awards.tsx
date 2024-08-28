2685676ac0a289d4cb48578256dd16c70128bb3f
Web-Mastermind <azeagent@gmail.com>
2024-08-28 23:58:03 +0400

    new section Awards

---
+++ src/components/aboutSectionAwards/Awards.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+import styles from "./style.module.css"
+import { useTranslations } from "next-intl"
+
+const Awards = () => {
+  const t = useTranslations("aboutSectionAwards")
+  return (
+    <section className={styles.sectionAwards}>
+      <div className="container">
+        <div className={styles.sectionItems}>
+          <div className={styles.sectionLeftItems}>
+            <p className={styles.sectionText}>{t("sectionText")}</p>
+            <h2 className={styles.sectionTitle}>{t("sectionTitle")}</h2>
+          </div>
+          <div className={styles.sectionRightItems}>
+            <Image className={styles.awardsImgs} src="/assets/img/Awards.png" alt="Awards" width={580} height={286} />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Awards
