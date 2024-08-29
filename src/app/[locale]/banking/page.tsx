c4292a2cc91d131b441adb8ff4ef2645dc8da637
Web-Mastermind <azeagent@gmail.com>
2024-08-30 00:42:36 +0400

    new page Banking

--- src/app/[locale]/banking/page.tsx
+++ src/app/[locale]/banking/page.tsx
@@ -1,20 +1,26 @@
 import ContactUs from "@/components/mainbutton/ContactUs"
 import styles from "./style.module.css"
+import { useTranslations } from "next-intl"
+import Account from "@/components/bankingSectionAccount/Account"
 
 const page = () => {
+  const t = useTranslations("bankingPage")
   return (
-    <section className={styles.bankingMain}>
-      <div className="container">
-        <div className={styles.pageItems}>
-          <h3 className={styles.pageTitle}>Taxus Banking</h3>
-          <h3 className={styles.pageSubTitle}>Partnering for Generations of Success</h3>
-          <p className={styles.lorem}>lorem ipsum dolor sit</p>
-          <div className={styles.contactItem}>
-            <ContactUs />
+    <>
+      <section className={styles.bankingMain}>
+        <div className="container">
+          <div className={styles.pageItems}>
+            <h3 className={styles.pageTitle}>{t("sectionTitle")}</h3>
+            <h3 className={styles.pageSubTitle}>{t("sectionSubTitle")}</h3>
+            <p className={styles.lorem}>lorem ipsum dolor sit</p>
+            <div className={styles.contactItem}>
+              <ContactUs />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <Account />
+    </>
   )
 }
 
