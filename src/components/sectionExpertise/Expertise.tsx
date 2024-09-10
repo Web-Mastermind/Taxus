759fb303662e4971ce2e76c00736545eaea4aab4
Web-Mastermind <azeagent@gmail.com>
2024-09-10 23:30:41 +0400

    don't repeat yourself

--- src/components/sectionExpertise/Expertise.tsx
+++ src/components/sectionExpertise/Expertise.tsx
@@ -9,9 +9,9 @@ const Expertise = () => {
   return (
     <section className={styles.expertiseSection}>
       <div className="container">
-        <div className={styles.sectionItems}>
+        <div className={`${styles.sectionItems} ${styles.colorWhite}`}>
           <p className={styles.expertiseText}>{t("SectionFirstTexts.expertiseMainText")}</p>
-          <h2 className={styles.expertiseTitle}>{t("SectionFirstTexts.expertiseMainTitle")}</h2>
+          <h2 className={`${styles.expertiseTitle} ${styles.fontMregular}`}>{t("SectionFirstTexts.expertiseMainTitle")}</h2>
         </div>
         <div className={styles.sectionSecondItemsContainer}>
           {
@@ -19,8 +19,8 @@ const Expertise = () => {
               return (
                 <div key={id} className={styles.sectionSecondItems}>
                   <Image className={styles.sectionicons} src={image} alt={alt} width={90} height={90} />
-                  <h3 className={styles.sectionSubTitle}>{title}</h3>
-                  <p className={styles.sectionSubText}>{text}</p>
+                  <h3 className={`${styles.sectionSubTitle} ${styles.colorWhite}`}>{title}</h3>
+                  <p className={`${styles.sectionSubText} ${styles.fontMregular}`}>{text}</p>
                 </div>
               )
             })
