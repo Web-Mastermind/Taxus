a7ed8d58960b78e99010d9a2841311e65594db13
Web-Mastermind <azeagent@gmail.com>
2024-09-11 19:52:13 +0400

    don't repeat yourself

--- src/components/sectionReviews/Reviews.tsx
+++ src/components/sectionReviews/Reviews.tsx
@@ -12,21 +12,21 @@ const Reviews = () => {
       <div className={styles.sectionItems}>
         <div className="container">
           <div className={styles.reviewsTextsItems}>
-            <p className={styles.sectionText}>{t("sectionText")}</p>
-            <h2 className={styles.sectionTitle}>{t("sectionTitle")}</h2>
+            <p className={`${styles.sectionText} ${styles.textCenter}`}>{t("sectionText")}</p>
+            <h2 className={`${styles.sectionTitle} ${styles.textCenter}`}>{t("sectionTitle")}</h2>
           </div>
         </div>
       </div>
       <div className={styles.reviewsContainer}>
         <div className="container">
-          <div className={styles.customersContainer}>
+          <div className={`${styles.customersContainer} ${styles.flex}`}>
             {
               review.map(({ id, text, name, job }) => {
                 return (
                   <div key={id} className={styles.customers}>
                     <p className={styles.reviewsIcon}>“</p>
                     <p className={styles.reviewsText}>{text}</p>
-                    <div className={styles.customersImg}>
+                    <div className={`${styles.customersImg} ${styles.flex}`}>
                       <Image className={styles.reviewsImage} src="/assets/icons/reviewIcon.svg" alt="review image" width={68} height={68} />
                       <div className={styles.reviewNameText}>
                         <p className={styles.reviewName}>{name}</p>
