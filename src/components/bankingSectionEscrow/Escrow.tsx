b170278a1a11c81a7c378f272a5ba1f909cfd8e9
Web-Mastermind <azeagent@gmail.com>
2024-09-12 00:17:18 +0400

    don't repeat yourself

--- src/components/bankingSectionEscrow/Escrow.tsx
+++ src/components/bankingSectionEscrow/Escrow.tsx
@@ -11,28 +11,28 @@ const Escrow = () => {
     <section className={styles.sectionEscrow}>
       <div className="container">
         <div className={styles.sectionItems}>
-          <div className={styles.sectionTextItems}>
-            <p className={styles.sectionText}>{t("aboutEscrowText")}</p>
+          <div className={`${styles.sectionTextItems} ${styles.textCenter}`}>
+            <p className={`${styles.sectionText} ${styles.colorWhite}`}>{t("aboutEscrowText")}</p>
             <h2 className={styles.sectionTitle}>{t("aboutEscrowTitle")}</h2>
             <p className={styles.sectionSubText}>{t("aboutEscrowSubTitle")}</p>
           </div>
-          <div className={styles.sectionCardsItems}>
+          <div className={`${styles.sectionCardsItems} ${styles.textCenter} ${styles.flex}`}>
             {
               escrow.map(({ id, img, title, text }) => {
                 return (
                   <div key={id} className={styles.sectionCards}>
-                    <h3 className={styles.cardTitle}>{title}</h3>
-                    <p className={styles.cardText}>{text}</p>
+                    <h3 className={`${styles.cardTitle} ${styles.colorDarkGrey}`}>{title}</h3>
+                    <p className={`${styles.cardText} ${styles.colorDarkGrey}`}>{text}</p>
                     <Image className={styles.cardIcon} src={img} alt="cards icon" width={68.22} height={80} />
                   </div>
                 )
               })
             }
           </div>
-          <div className={styles.sectionSubTextItems}>
-            <p className={styles.sectionFooterText}>{t("aboutEscrowSubText")}</p>
+          <div className={`${styles.sectionSubTextItems} ${styles.textCenter}`}>
+            <p className={`${styles.sectionFooterText} ${styles.colorWhite}`}>{t("aboutEscrowSubText")}</p>
           </div>
-          <div className={styles.contactItem}>
+          <div className={`${styles.contactItem} ${styles.flex}`}>
             <ContactUs />
           </div>
         </div>
