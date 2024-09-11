8147254dee0f2edffcb38de6073f97a73e80f1b6
Web-Mastermind <azeagent@gmail.com>
2024-09-11 23:53:48 +0400

    don't repeat yourself

--- src/components/bankingSectionAccount/Account.tsx
+++ src/components/bankingSectionAccount/Account.tsx
@@ -13,31 +13,31 @@ const Account = () => {
       <section className={styles.sectionAccount}>
         <div className="container">
           <div className={styles.sectionItems}>
-            <div className={styles.cardsContainer}>
+            <div className={`${styles.cardsContainer} ${styles.flex}`}>
               {
                 account.map(({ id, img, title, text }) => {
                   return (
                     <div key={id} className={styles.cards}>
-                      <h4 className={styles.cardTitle}>{title}</h4>
-                      <p className={styles.cardText}>{text}</p>
+                      <h4 className={`${styles.cardTitle} ${styles.colorDarkGrey}`}>{title}</h4>
+                      <p className={`${styles.cardText} ${styles.colorDarkGrey}`}>{text}</p>
                       <Image className={styles.cardIcon} src={img} alt="card icons" width={90} height={90} />
                     </div>
                   )
                 })
               }
             </div>
-            <div className={styles.sectionTopTexts}>
+            <div className={`${styles.sectionTopTexts} ${styles.flex}`}>
               <div className={styles.sectionTextItems}>
-                <p className={styles.sectionText}>{t("aboutCardsText")}</p>
-                <h2 className={styles.sectionTitle}>{t("aboutCardsTitle")}</h2>
+                <p className={`${styles.sectionText} ${styles.colorDarkGrey}`}>{t("aboutCardsText")}</p>
+                <h2 className={`${styles.sectionTitle} ${styles.colorPrimary}`}>{t("aboutCardsTitle")}</h2>
               </div>
               <div className={styles.sectionTopRightTextsContainer}>
-                <h2 className={styles.sectionRightTitle}>{t("aboutCardsSubTitle")}</h2>
+                <h2 className={`${styles.sectionRightTitle} ${styles.colorDarkGrey}`}>{t("aboutCardsSubTitle")}</h2>
                 {
                   rigtTexts.map(({ id, title, text }) => {
                     return (
-                      <div key={id} className={styles.sectionTopRightTextsItems}>
-                        <span className={styles.diamondIcon}>&#9670;</span>
+                      <div key={id} className={`${styles.sectionTopRightTextsItems} ${styles.flex}`}>
+                        <span className={`${styles.diamondIcon} ${styles.colorPrimary}`}>&#9670;</span>
                         <div className={styles.sectionRightTexts}>
                           <p className={styles.rightTextItems}>{title}</p>
                           <p className={styles.rightTextSubItems}>{text}</p>
