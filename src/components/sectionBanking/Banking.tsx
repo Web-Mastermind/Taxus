2edc45cdb4bdacace7525fce8ee6b435c943e3b2
Web-Mastermind <azeagent@gmail.com>
2024-09-11 14:38:36 +0400

    don't repeat yourself

--- src/components/sectionBanking/Banking.tsx
+++ src/components/sectionBanking/Banking.tsx
@@ -10,23 +10,23 @@ const Banking = () => {
   const banking = bankingSection();
   return (
     <article className={styles.sectionBanking}>
-      <div className={styles.sectionItems}>
+      <div className={`${styles.sectionItems} ${styles.flex}`}>
         <Image className={styles.bankingImage} src={sectionImage} alt="phone and credit card image" />
         <div className="container">
           <div className={styles.bankingTexts}>
-            <p className={styles.bankingMainText}>{t("sectionText")}</p>
-            <h3 className={styles.bankingMainTitle}>{t("sectionTitle")}</h3>
+            <p className={`${styles.bankingMainText} ${styles.colorDarkGrey}`}>{t("sectionText")}</p>
+            <h3 className={`${styles.bankingMainTitle} ${styles.colorPrimary}`}>{t("sectionTitle")}</h3>
             {
               banking.map(({ id, icon, text }) => {
                 return (
-                  <div key={id} className={styles.bankingTextContainer}>
-                    <span className={styles.textDecor}>{icon}</span>
-                    <p className={styles.bankingSubTexts}>{text}</p>
+                  <div key={id} className={`${styles.bankingTextContainer} ${styles.flex}`}>
+                    <span className={`${styles.textDecor} ${styles.colorPrimary}`}>{icon}</span>
+                    <p className={`${styles.bankingSubTexts} ${styles.colorDarkGrey}`}>{text}</p>
                   </div>
                 )
               })
             }
-            <Link className={styles.bankingLinkItem} href="/">{t("bankingLink")} &rarr;</Link>
+            <Link className={`${styles.bankingLinkItem} ${styles.colorDarkGrey}`} href="/">{t("bankingLink")} &rarr;</Link>
           </div>
         </div>
       </div>
