95e0fc4c5c568c7cba3479256ebe40e6da4870da
Web-Mastermind <azeagent@gmail.com>
2024-09-12 00:09:20 +0400

    don't repeat yourself

--- src/components/bankingSectionPrivate/Private.tsx
+++ src/components/bankingSectionPrivate/Private.tsx
@@ -9,19 +9,19 @@ const Private = () => {
   return (
     <section className={styles.sectionPrivate}>
       <div className="container">
-        <div className={styles.sectionItems}>
+        <div className={`${styles.sectionItems} ${styles.flex}`}>
           <div className={styles.sectionLeftContainer}>
-            <p className={styles.sectionLeftText}>{t("aboutPrivateText")}</p>
-            <h2 className={styles.sectionLeftTitle}>{t("aboutPrivateTitle")}</h2>
+            <p className={`${styles.sectionLeftText} ${styles.colorDarkGrey}`}>{t("aboutPrivateText")}</p>
+            <h2 className={`${styles.sectionLeftTitle} ${styles.colorPrimary}`}>{t("aboutPrivateTitle")}</h2>
             <p className={styles.sectionLeftSubTitles}>{t("aboutPrivateSubTitle")}</p>
           </div>
           <div className={styles.sectionRightContainer}>
-            <h3 className={styles.sectionRightTitle}>{t("aboutPrivateSubText")}</h3>
+            <h3 className={`${styles.sectionRightTitle} ${styles.colorDarkGrey}`}>{t("aboutPrivateSubText")}</h3>
             {
               privat.map(({ id, title, text }) => {
                 return (
-                  <div key={id} className={styles.sectionRightMainItems}>
-                    <span className={styles.diamondIcon}>&#9670;</span>
+                  <div key={id} className={`${styles.sectionRightMainItems} ${styles.flex}`}>
+                    <span className={`${styles.diamondIcon} ${styles.colorPrimary}`}>&#9670;</span>
                     <div className={styles.sectionRightTexts}>
                       <p className={styles.rightTextItems}>{title}</p>
                       <p className={styles.rightTextSubItems}>{text}</p>
