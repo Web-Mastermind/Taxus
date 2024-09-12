494e9006b0b158984e970055e05173f91cf83096
Web-Mastermind <azeagent@gmail.com>
2024-09-12 23:53:18 +0400

    don't repeat yourself

--- src/components/aboutSectionReview/AboutReview.tsx
+++ src/components/aboutSectionReview/AboutReview.tsx
@@ -9,29 +9,29 @@ const AboutReview = () => {
   const t2 = useTranslations("aboutSectionBenefits")
   const review = reviewsSection();
   return (
-    <section className={styles.aboutSectionReview}>
-      <div className={styles.sectionItems}>
+    <section className={`${styles.aboutSectionReview} ${styles.relative}`}>
+      <div className={`${styles.sectionItems} ${styles.relative}`}>
         <div className="container">
           <div className={styles.reviewsTextsItems}>
-            <p className={styles.sectionText}>{t("sectionText")}</p>
-            <h2 className={styles.sectionTitle}>{t("sectionTitle")}</h2>
+            <p className={`${styles.sectionText} ${styles.textCenter}`}>{t("sectionText")}</p>
+            <h2 className={`${styles.sectionTitle} ${styles.textCenter}`}>{t("sectionTitle")}</h2>
           </div>
         </div>
       </div>
       <div className={styles.reviewsContainer}>
         <div className="container">
-          <div className={styles.customersContainer}>
+          <div className={`${styles.customersContainer} ${styles.flex}`}>
             {
               review.map(({ id, text, name, job }) => {
                 return (
                   <div key={id} className={styles.customers}>
                     <p className={styles.reviewsIcon}>“</p>
-                    <p className={styles.reviewsText}>{text}</p>
-                    <div className={styles.customersImg}>
+                    <p className={`${styles.reviewsText} ${styles.fontAll}`}>{text}</p>
+                    <div className={`${styles.customersImg} ${styles.flex}`}>
                       <Image className={styles.reviewsImage} src="/assets/icons/reviewIcon.svg" alt="review image" width={68} height={68} />
                       <div className={styles.reviewNameText}>
                         <p className={styles.reviewName}>{name}</p>
-                        <p className={styles.reviewJob}>{job}</p>
+                        <p className={`${styles.reviewJob} ${styles.fontAll}`}>{job}</p>
                       </div>
                     </div>
                   </div>
