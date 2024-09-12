4f9f5b3fb21384cda49be920d53e03f83353a554
Web-Mastermind <azeagent@gmail.com>
2024-09-12 23:42:43 +0400

    don't repeat yourself

--- src/components/aboutSectionHistory/History.tsx
+++ src/components/aboutSectionHistory/History.tsx
@@ -12,27 +12,27 @@ const History = () => {
     <section className={styles.sectionHistory}>
       <div className="container">
         <div className={styles.sectionItems}>
-          <p className={styles.sectionText}>{t("sectionText")}</p>
-          <h2 className={styles.sectionTitle}>{t("sectionTitle")}</h2>
-          <p className={styles.sectionSubText}>{t("sectionSubText")}</p>
-          <p className={styles.sectionSubText}>{t("sectionSubText2")}</p>
+          <p className={`${styles.sectionText} ${styles.colorWhite} ${styles.textCenter}`}>{t("sectionText")}</p>
+          <h2 className={`${styles.sectionTitle} ${styles.textCenter}`}>{t("sectionTitle")}</h2>
+          <p className={`${styles.sectionSubText} ${styles.colorWhite}`}>{t("sectionSubText")}</p>
+          <p className={`${styles.sectionSubText} ${styles.colorWhite}`}>{t("sectionSubText2")}</p>
           <div className={styles.sectionSubItems}>
-            <p className={styles.sectionSecondText}>{t2("aboutBenefitsText")}</p>
-            <h2 className={styles.sectionTitle}>{t2("aboutBenefitsTitle")}</h2>
-            <div className={styles.benefitsContainer}>
+            <p className={`${styles.sectionSecondText} ${styles.colorWhite} ${styles.textCenter}`}>{t2("aboutBenefitsText")}</p>
+            <h2 className={`${styles.sectionTitle} ${styles.textCenter}`}>{t2("aboutBenefitsTitle")}</h2>
+            <div className={`${styles.benefitsContainer} ${styles.flex}`}>
               {
                 History.map(({ id, img, title, text }) => {
                   return (
                     <div key={id} className={styles.sectionBenefitsItems}>
                       <Image className={styles.sectionIcon} src={img} alt="Team icon" width={55.85} height={60} />
-                      <h2 className={styles.sectionBenefitsTitle}>{title} </h2>
+                      <h2 className={`${styles.sectionBenefitsTitle} ${styles.colorWhite}`}>{title} </h2>
                       <p className={styles.sectionBenefitsText}>{text}</p>
                     </div>
                   )
                 })
               }
             </div>
-            <div className={styles.contactItem}>
+            <div className={`${styles.contactItem} ${styles.flex}`}>
               <ContactUs />
             </div>
           </div>
