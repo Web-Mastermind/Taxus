1b820e718300cadc04d6444b9176731e9d6cdbe4
Web-Mastermind <azeagent@gmail.com>
2024-09-13 23:12:53 +0400

    translated

--- src/components/companySectionCorporate/Corporate.tsx
+++ src/components/companySectionCorporate/Corporate.tsx
@@ -1,19 +1,22 @@
 import Image from "next/image"
 import styles from "./style.module.css"
 import { CompanySectionCorporate } from "@/links/Links"
+import { useTranslations } from "next-intl"
 
 const Corporate = () => {
+  const t = useTranslations("CompanyPage");
+  const CORPORATE = CompanySectionCorporate();
   return (
     <section className={styles.sectionCorporate}>
       <div className="container">
         <div className={styles.sectionItems}>
           <div className={styles.sectionTextItems}>
-            <p className={`${styles.sectionText} ${styles.colorWhite}`}>Corporate structure</p>
-            <h2 className={styles.sectionTitle}>Benefits of Proper Company Set Up</h2>
+            <p className={`${styles.sectionText} ${styles.colorWhite}`}>{t("CompanySubPageText")}</p>
+            <h2 className={styles.sectionTitle}>{t("CompanySubPageTitle")}</h2>
           </div>
           <div className={styles.sectionSubItems}>
             {
-              CompanySectionCorporate.map(({ id, title, text, subText }) => {
+              CORPORATE.map(({ id, title, text, subText }) => {
                 return (
                   <div key={id} className={styles.sectionSubText}>
                     <h3 className={`${styles.sectionSubTitle} ${styles.colorWhite}`}>{title}</h3>
