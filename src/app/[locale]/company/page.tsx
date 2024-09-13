8eeb00a6b6904298d0b815bb31bad960952e73a2
Web-Mastermind <azeagent@gmail.com>
2024-09-13 23:12:41 +0400

    translated

--- src/app/[locale]/company/page.tsx
+++ src/app/[locale]/company/page.tsx
@@ -2,15 +2,17 @@ import ContactUs from "@/components/mainbutton/ContactUs"
 import styles from "./style.module.css"
 import Image from "next/image"
 import Corporate from "@/components/companySectionCorporate/Corporate"
+import { useTranslations } from "next-intl"
 
 const Company = () => {
+  const t = useTranslations("CompanyPage");
   return (
     <section className={styles.CompanyPage}>
       <div className="container">
         <div className={styles.pageItems}>
           <div className={styles.pageTextItems}>
-            <h2 className={styles.pageTitle}>Company Registration and Maintenance</h2>
-            <p className={styles.pageText}>If you’re running an international IT business, having a properly set up company can provide many benefits, including liability protection, tax advantages, and increased credibility with clients and partners. At Taxus, we specialize in helping businesses set up and maintain their companies in countries around the world. Here are some of the ways we can help you.</p>
+            <h2 className={styles.pageTitle}>{t("CompanyPageText")}</h2>
+            <p className={styles.pageText}>{t("CompanyPageTitle")}</p>
             <div className={styles.buttonContainer}>
               <ContactUs />
             </div>
